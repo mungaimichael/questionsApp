03b2b7734b4103b0b6df0bbeb36c864384f92e45
mungaimichael <mungaimikey@gmail.com>
2024-11-04 16:39:37 +0300

    added edit and create functionality

--- src/main/java/com/learning/questionsApp/controllers/QuestionsController.java
+++ src/main/java/com/learning/questionsApp/controllers/QuestionsController.java
@@ -3,11 +3,10 @@ package com.learning.questionsApp.controllers;
 import com.learning.questionsApp.entity.Question;
 import com.learning.questionsApp.services.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("question")
@@ -15,13 +14,33 @@ public class QuestionsController {
 
     @Autowired
     QuestionService questionService;
-    @GetMapping("/allQuestions")
 
-    public List<Question> getAllQuestions () {
+    @GetMapping("/allQuestions")
+    public List<Question> getAllQuestions() {
         return questionService.getAllQuestions();
     }
 
+    // Get a single question
+    @GetMapping("/questions/{id}")
+    public Optional<Question> getSingleQuestion(@PathVariable int id) {
+        return questionService.getSingleQuestion(id);
+    }
 
+    // POST controller for user to add a custom question to db
+    @PostMapping("/create")
+    public String createQuestion(@RequestBody Question question) {
+        return questionService.createQuestion(question);
+    }
 
+    //  DELETE mapping
+    @DeleteMapping("/delete/{id}")
+    public String deleteQuestion(@PathVariable int id) {
+        return questionService.deleteQuestion(id);
+    }
 
+    // Edit a question
+    @PutMapping("edit/{id}")
+    public String updateQuestion(@PathVariable int id, @RequestBody Question question) {
+        return questionService.updateQuestion(question);
+    }
 }
