506bdb251939716c83fa6b3f47cf8af88806372c
mungaimichael <mungaimikey@gmail.com>
2024-11-04 11:45:09 +0300

    init commit

---
+++ src/main/java/com/learning/questionsApp/QuestionsAppApplication.java
@@ -0,0 +1,13 @@
+package com.learning.questionsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionsAppApplication.class, args);
+	}
+
+}
