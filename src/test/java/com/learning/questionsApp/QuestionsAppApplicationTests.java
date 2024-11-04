0a169492eed0d53527c9de034b1db7fdaf046c65
mungaimichael <mungaimikey@gmail.com>
2024-11-04 11:45:16 +0300

    init commit

---
+++ src/test/java/com/learning/questionsApp/QuestionsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.questionsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
