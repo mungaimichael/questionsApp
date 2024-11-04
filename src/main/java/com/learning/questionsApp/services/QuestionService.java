91f8e5e0558b834c3ab340623d13596d08082714
mungaimichael <mungaimikey@gmail.com>
2024-11-04 16:40:07 +0300

    added edit and create functionality

--- src/main/java/com/learning/questionsApp/services/QuestionService.java
+++ src/main/java/com/learning/questionsApp/services/QuestionService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuestionService {
@@ -15,4 +16,33 @@ public class QuestionService {
    public List<Question> getAllQuestions() {
         return questionRepo.findAll();
     }
+
+    public Optional<Question> getSingleQuestion (int id) {
+       return questionRepo.findById(id);
+    }
+
+    public  String createQuestion(Question question) {
+       questionRepo.save(question);
+       return "Question created";
+    }
+
+    // delete question from db
+    public String deleteQuestion(int id) {
+
+        if (questionRepo.existsById(id)) { // Check if the question exists
+            questionRepo.deleteById(id);
+            return "Question deleted";
+        } else {
+            return "Question not found";
+        }
+    }
+
+    public String updateQuestion(Question question) {
+       // check if question exits then edit the fields
+       if (questionRepo.existsById(question.getId())) {
+           questionRepo.save(question);
+       }
+       return "Question updated";
+    }
+
 }
