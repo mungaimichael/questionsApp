6895b8f932665691858ebe0f906e42a16ee2be37
mungaimichael <mungaimikey@gmail.com>
2024-11-09 21:00:06 +0300

    chore:allowed CORS

--- src/main/java/com/learning/questionsApp/controllers/QuestionsController.java
+++ src/main/java/com/learning/questionsApp/controllers/QuestionsController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
-@RequestMapping("question")
+@RequestMapping("/api/v1/question")
 public class QuestionsController {
 
     @Autowired
