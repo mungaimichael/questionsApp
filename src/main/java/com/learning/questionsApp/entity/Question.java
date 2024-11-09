faff10a25aff98ec2f3ad2f75a029b97395571e2
mungaimichael <mungaimikey@gmail.com>
2024-11-09 21:00:31 +0300

    chore:made options into different table

--- src/main/java/com/learning/questionsApp/entity/Question.java
+++ src/main/java/com/learning/questionsApp/entity/Question.java
@@ -1,22 +1,26 @@
 package com.learning.questionsApp.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Data
 @Entity
 public class Question {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private  Integer id;
+    private Integer id;
+
     private String questionTitle;
-    private String option1;
-    private String option2;
-    private String option3;
-    private String option4;
-    private  String difficultyLevel;
+
+    @ElementCollection
+    private List<String> questionOptions;
+
+    private String difficultyLevel;
+
     private String correctAnswer;
 }
