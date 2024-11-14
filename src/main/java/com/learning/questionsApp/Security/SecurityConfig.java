af997e4d74eded08e6e3c6b91919223c162078d6
mungaimichael <mungaimikey@gmail.com>
2024-11-14 12:17:53 +0300

    chore:filter request

---
+++ src/main/java/com/learning/questionsApp/Security/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.learning.questionsApp.Security;
+
+import com.learning.questionsApp.repository.UserRepo;
+import com.learning.questionsApp.services.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private JWTFilter jwtFilter;
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(basic -> basic.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/v1/question/allQuestions").hasRole("USER")
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
