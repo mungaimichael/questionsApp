5887513381db6555645d1c9daa2f4ce002340f76
mungaimichael <mungaimikey@gmail.com>
2024-11-14 12:18:07 +0300

    refactor

--- src/main/java/com/learning/questionsApp/entity/User.java
+++ src/main/java/com/learning/questionsApp/entity/User.java
@@ -6,7 +6,7 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "user_table")
-public class User {
+public class User{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int user_id;
